72cf962d956457e1cca850658434751683ee4623
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-07-20 13:44:27 -0400

    added eventlisteners for seat selection and a change in movie selection. Also added a function to update the total.

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
+// updates the seats selected and the total
+const updateCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedCount = selectedSeats.length;
+
+  count.innerText = selectedCount
+  total.innerText = selectedCount * ticketPrice;
+}
+
+// handles a switch in movie selection
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+
+  updateCount();
+})
+
+
+// click handler for selecting seats
 container.addEventListener('click', (e) => {
-  if(e.target.classList.contains('seat') && !e.target.classList.conatins('occupied')){
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
     e.target.classList.toggle('selected');
+
+    updateCount();
   }
 })
+
+
