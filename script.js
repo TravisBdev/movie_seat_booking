1d7441f7f9b28955e7678d85c4bafd9bf7022ac0
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-07-21 10:49:11 -0400

    added local storage

--- script.js
+++ script.js
@@ -5,19 +5,54 @@ const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
 let ticketPrice = +movieSelect.value;
 
+
+
+// saves the selected movie including the price
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // updates the seats selected and the total
 const updateCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedCount = selectedSeats.length;
 
-  count.innerText = selectedCount
+  // saves the index of the selected seats
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  })
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  count.innerText = selectedCount;
   total.innerText = selectedCount * ticketPrice;
 }
 
-// handles a switch in movie selection
+// populate UI with data from localstorage
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if(selectedSeats !== null && selectedSeats.length > 0){
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1){
+        seat.classList.add('selected');
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null){
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+
+// handles a change in movie selection
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
-
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateCount();
 })
 
@@ -30,5 +65,8 @@ container.addEventListener('click', (e) => {
     updateCount();
   }
 })
+
+// initial count and total set
+updateCount();
 
 
